d486b176a4a50242e636917465c792fb1e9b4f9e
hhimanshu <harit.himanshu@yahoo.in>
2017-05-04 06:10:40 +1200

    Not able to complete the workflow

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,12 +1,23 @@
+/* global gapi */
 import React from 'react';
 
-var LoginButton = React.createClass({
 
-    onSignIn: function(googleUser) {
+class LoginButton extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onSignIn = this.onSignIn.bind(this);
+        //this.renderGoogleLoginButton = this.renderGoogleLoginButton.bind(this);
+    }
+
+
+    onSignIn(googleUser) {
         console.log("user signed in"); // plus any other logic here
-    },
+    }
 
-    renderGoogleLoginButton: function() {
+/*
+    renderGoogleLoginButton() {
         console.log('rendering google signin button');
         gapi.signin2.render('my-signin2', {
             'scope': 'https://www.googleapis.com/auth/plus.login',
@@ -16,19 +27,32 @@ var LoginButton = React.createClass({
             'theme': 'light',
             'onsuccess': this.onSignIn
         })
-    },
+    }
+*/
 
-    componentDidMount: function() {
-        window.addEventListener('google-loaded',this.renderGoogleLoginButton);
-    },
+    componentDidMount() {
+        //window.addEventListener('google-loaded', this.renderGoogleLoginButton);
+        gapi.load('auth2', () => {
+            // Retrieve the singleton for the GoogleAuth library and set up the client.
+            this.auth2 = gapi.auth2.init({
+                client_id: '793065510867-j06sr2rufh7ns1kepcdolt9l22ph5pso.apps.googleusercontent.com'
+            });
 
-    render: function() {
-        let displayText = "Sign in with Google";
+ /*           this.auth2.attachClickHandler(this.refs.googleButton, {},
+                (googleUser) => {
+                    this.googleLogin(googleUser.getBasicProfile());
+                }, (error) => {
+
+                });
+*/        });
+    }
+
+    render() {
         return (
-            <div class="g-signin2" data-onsuccess="onSignIn"></div>
+            <div className="g-signin2" data-onsuccess={this.onSignIn}></div>
         );
     }
 
-});
+}
 
 export default LoginButton;
