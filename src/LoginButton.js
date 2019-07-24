1e0cd4a2ab79f5ed0f1a62917c79fb6a90e079cb
hhimanshu <harit.himanshu@yahoo.in>
2019-07-24 12:00:48 -0700

    Make login/log out work as expected

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -3,28 +3,30 @@ import React from 'react';
 
 
 class LoginButton extends React.Component {
-
     constructor(props) {
         super(props);
-
-        this.onSignIn = this.onSignIn.bind(this);
-        this.renderGoogleLoginButton = this.renderGoogleLoginButton.bind(this);
-        this.logout = this.logout.bind(this);
+        this.state = {
+            isLoggedIn: false
+        }
     }
 
+    toggleLoggedIn = () => this.setState((state) => {
+        return {isLoggedIn: !state.isLoggedIn}
+    });
 
-    onSignIn(googleUser) {
-        console.log("user signed in");
+    onSignIn = (googleUser) => {
+        this.toggleLoggedIn()
+
         let user = googleUser.getBasicProfile();
         let id_token = googleUser.getAuthResponse().id_token;
+
         console.log('google user obj', user);
         console.log('google_id_token', id_token);
-
         // plus any other logic here
-    }
+    };
 
 
-    renderGoogleLoginButton() {
+    renderGoogleLoginButton = () => {
         console.log('rendering google signin button');
         window.gapi.signin2.render('my-signin2', {
             'scope': 'profile email',
@@ -34,19 +36,20 @@ class LoginButton extends React.Component {
             'theme': 'light',
             'onsuccess': this.onSignIn
         })
-    }
+    };
 
-    logout() {
+    logout = () => {
         console.log('in logout');
+
         let auth2 = window.gapi && window.gapi.auth2.getAuthInstance();
         if(auth2) {
             auth2.signOut()
-            .then(() => {console.log('Logged out successfully')})
+            .then(() => {this.toggleLoggedIn(); console.log('Logged out successfully')})
             .catch(err => {console.log('Error while logging out', err)});
         } else {
             console.log('error while logging out');
         }
-    }
+    };
 
     componentDidMount() {
         window.addEventListener('google-loaded', this.renderGoogleLoginButton);
@@ -54,17 +57,19 @@ class LoginButton extends React.Component {
     }
 
     render() {
+        // noinspection CheckTagEmptyBody
         return (
           <div>
             <div id='my-signin2'></div>
             <br />
-            <button style={{width: 200, height: 50, textAlign: 'center'}} onClick={this.logout}>
-                Logout
-            </button>
+              {this.state.isLoggedIn &&
+              <button style={{width: 200, height: 50, textAlign: 'center'}} onClick={this.logout}>
+                  Logout
+              </button>
+              }
           </div>
         );
     }
-
 }
 
 export default LoginButton;
